ec4b9b5eed9735d15dfdd17277a2eb25caeba9b9
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-24 15:27:54 +0900

    [ADD] Initial Commit

---
+++ src/main/java/com/uliga/uliga_backend/UligaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uliga.uliga_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UligaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UligaBackendApplication.class, args);
+    }
+
+}
