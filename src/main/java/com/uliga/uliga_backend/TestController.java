9a2934c08380f2777363feab770f1a91e1e90d31
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-24 22:34:44 +0900

    [ADD] 배포 테스트를 위한 임시 컨트롤러 구현 ULIGA-40

---
+++ src/main/java/com/uliga/uliga_backend/TestController.java
@@ -0,0 +1,13 @@
+package com.uliga.uliga_backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/test")
+    public String test() {
+        return "test";
+    }
+}
