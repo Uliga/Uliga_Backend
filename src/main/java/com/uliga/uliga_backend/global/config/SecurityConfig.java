e720cac9597e4f9262701088cf18c6a102e6c460
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-03-05 17:16:21 +0900

    [ADD] Cors 해결을 위한 SecurityConfig 수정

--- src/main/java/com/uliga/uliga_backend/global/config/SecurityConfig.java
+++ src/main/java/com/uliga/uliga_backend/global/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("localhost:3000");
+        configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
