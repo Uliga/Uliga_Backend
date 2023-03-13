497f5cdfe638ca5b4115cc10a4cab991bc4f6a48
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-03-13 14:24:23 +0900

    [ADD] UserDetailServiceTest 작성

---
+++ src/test/java/com/uliga/uliga_backend/domain/Member/application/UserDetailServiceTest.java
@@ -0,0 +1,60 @@
+package com.uliga.uliga_backend.domain.Member.application;
+
+import com.uliga.uliga_backend.domain.Member.dto.MemberDTO;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static com.uliga.uliga_backend.domain.Member.dto.MemberDTO.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class UserDetailServiceTest {
+    @Autowired
+    UserDetailService userDetailService;
+    @Autowired
+    AuthService authService;
+
+    public SignUpRequest createSignUpRequest(String email, String nickname) {
+        return SignUpRequest.builder()
+                .email(email)
+                .applicationPassword("1234")
+                .nickName(nickname)
+                .password("12345678")
+                .userName("testUser")
+                .build();
+    }
+
+    @Test
+    @DisplayName("이메일로 UserDetail 조회 성공")
+    public void getUserDetailByEmailToSuccess() throws Exception{
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest("email@email.com", "testNickname");
+
+        // when
+        Long aLong = authService.signUp(signUpRequest);
+
+        // then
+        UserDetails userDetails = userDetailService.loadUserByUsername("email@email.com");
+        assertEquals(Long.parseLong(userDetails.getUsername()), aLong);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 이메일로 userDetail 조회 실패")
+    public void getUserDetailByEmailToFail() throws Exception{
+        //given
+        SignUpRequest signUpRequest = createSignUpRequest("email@email.com", "testNickname");
+
+        // when
+        Long aLong = authService.signUp(signUpRequest);
+
+        // then
+        assertThrows(UsernameNotFoundException.class, () -> userDetailService.loadUserByUsername("email2@email.com"));
+    }
+
+}
