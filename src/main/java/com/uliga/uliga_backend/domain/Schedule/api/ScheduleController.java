9a6bef96e34e34e031bd33eac32193b7d64b5946
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-03-16 13:53:40 +0900

    [ADD] 예산/금융 일정 Swagger 태그 추가

--- src/main/java/com/uliga/uliga_backend/domain/Schedule/api/ScheduleController.java
+++ src/main/java/com/uliga/uliga_backend/domain/Schedule/api/ScheduleController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-
+@Tag(name = "금융 일정", description = "금융 일정 관련 API 입니다.")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
