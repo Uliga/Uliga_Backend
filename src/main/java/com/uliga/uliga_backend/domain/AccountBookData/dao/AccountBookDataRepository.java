0044cb0e1a8a3fefb78c21ec65c707c712c89148
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-03-28 23:44:32 +0900

    [ADD] 가계부 수입/지출 내역 조회 구현 (년도 별, 월별 조회 가능)

--- src/main/java/com/uliga/uliga_backend/domain/AccountBookData/dao/AccountBookDataRepository.java
+++ src/main/java/com/uliga/uliga_backend/domain/AccountBookData/dao/AccountBookDataRepository.java
@@ -9,11 +9,56 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AccountBookDataRepository extends JpaRepository<AccountBookData, Long> {
-    @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.AccountBookDataQ(abd.id, abd.value, abd.payment, abd.account, abd.memo, abd.date.year, abd.date.month, abd.date.day, abd.type, abd.creator.userName, abd.category.name) FROM AccountBookData abd LEFT JOIN Income i on i.id = abd.id LEFT JOIN Record r ON r.id = abd.id")
+    @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.AccountBookDataQ(" +
+            "abd.id, " +
+            "abd.value, " +
+            "abd.payment, " +
+            "abd.account, " +
+            "abd.memo, " +
+            "abd.date.year, " +
+            "abd.date.month, " +
+            "abd.date.day, " +
+            "abd.type, " +
+            "abd.creator.userName, " +
+            "abd.category.name) FROM AccountBookData abd " +
+            "LEFT JOIN Income i on i.id = abd.id " +
+            "LEFT JOIN Record r ON r.id = abd.id " +
+            "WHERE abd.accountBook.id = :id " +
+            "ORDER BY abd.date.year*365 + abd.date.month*31 + abd.date.day DESC ")
     Page<AccountBookDataQ> findAccountBookDataByAccountBookId(@Param("id") Long id, Pageable pageable);
-    @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.AccountBookDataQ(abd.id, abd.value, abd.payment, abd.account, abd.memo, abd.date.year, abd.date.month, abd.date.day, abd.type, abd.creator.userName, abd.category.name) FROM AccountBookData abd LEFT JOIN Income i on i.id = abd.id LEFT JOIN Record r ON r.id = abd.id")
+    @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.AccountBookDataQ(" +
+            "abd.id, " +
+            "abd.value, " +
+            "abd.payment, " +
+            "abd.account, " +
+            "abd.memo, " +
+            "abd.date.year, " +
+            "abd.date.month, " +
+            "abd.date.day, " +
+            "abd.type, " +
+            "abd.creator.userName, " +
+            "abd.category.name) FROM AccountBookData abd " +
+            "LEFT JOIN Income i on i.id = abd.id " +
+            "LEFT JOIN Record r ON r.id = abd.id " +
+            "WHERE abd.accountBook.id = :id AND abd.date.year = :year " +
+            "ORDER BY abd.date.year*365 + abd.date.month*31 + abd.date.day DESC ")
     Page<AccountBookDataQ> findAccountBookDataByAccountBookIdAndYear(@Param("id") Long id, @Param("year") Long year, Pageable pageable);
-    @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.AccountBookDataQ(abd.id, abd.value, abd.payment, abd.account, abd.memo, abd.date.year, abd.date.month, abd.date.day, abd.type, abd.creator.userName, abd.category.name) FROM AccountBookData abd LEFT JOIN Income i on i.id = abd.id LEFT JOIN Record r ON r.id = abd.id")
+    @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.AccountBookDataQ(" +
+            "abd.id, " +
+            "abd.value, " +
+            "abd.payment, " +
+            "abd.account, " +
+            "abd.memo, " +
+            "abd.date.year, " +
+            "abd.date.month, " +
+            "abd.date.day, " +
+            "abd.type, " +
+            "abd.creator.userName, " +
+            "abd.category.name) FROM AccountBookData abd " +
+            "LEFT JOIN Income i on i.id = abd.id " +
+            "LEFT JOIN Record r ON r.id = abd.id " +
+            "WHERE abd.accountBook.id = :id AND abd.date.year=:year AND abd.date.month=:month " +
+            "ORDER BY abd.date.year*365 + abd.date.month*31 + abd.date.day DESC ")
     Page<AccountBookDataQ> findAccountBookDataByAccountBookIdAndYearAndMonth(@Param("id") Long id, @Param("year") Long year, @Param("month") Long month, Pageable pageable);
 }
 
