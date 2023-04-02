4e985d53e0bbfbbc4f87d35572e9abf11f5e23d1
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-04-02 16:08:52 +0900

    [ADD] 금융일정 조회시, 지출, 수입 총합 추가

--- src/main/java/com/uliga/uliga_backend/domain/Schedule/dto/NativeQ/ScheduleMonthSum.java
+++ src/main/java/com/uliga/uliga_backend/domain/Schedule/dto/NativeQ/ScheduleMonthSum.java
@@ -1,13 +1,10 @@
 package com.uliga.uliga_backend.domain.Schedule.dto.NativeQ;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Data
 @Getter
-@NoArgsConstructor
 @Schema(description = "한달 금융일정 지출/수입 총합")
 public class ScheduleMonthSum {
     private Long value;
