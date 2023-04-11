53a5e989f26ef036d13a2dbe6c3261bb668f6d95
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-04-11 14:18:37 +0900

    [ADD] 이메일 인증 시간 변경

--- src/main/java/com/uliga/uliga_backend/global/common/constants/EmailConstants.java
+++ src/main/java/com/uliga/uliga_backend/global/common/constants/EmailConstants.java
@@ -1,5 +1,5 @@
 package com.uliga.uliga_backend.global.common.constants;
 
 public class EmailConstants {
-    public static final long EMAIL_CERTIFICATION_TIME = 1000 * 60 * 60 * 24; // 하루
+    public static final long EMAIL_CERTIFICATION_TIME = 1000 * 60 * 5; // 하루
 }
