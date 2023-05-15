f096528a6e09822299c33e5097234094f2222261
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-15 17:55:56 +0900

    [ADD] 이메일 인증 관련 값 변경

--- src/main/java/com/uliga/uliga_backend/domain/Member/application/EmailCertificationService.java
+++ src/main/java/com/uliga/uliga_backend/domain/Member/application/EmailCertificationService.java
@@ -52,63 +52,65 @@ public class EmailCertificationService {
 
 
         String msg = "";
-        msg += "<meta charset='UTF-8'>\n" +
-                "<table align='center' border='0' cellpadding='0' cellspacing='0' width='100%'\n" +
-                "       style='padding:60px 0 60px 0;color:#555;font-size:16px;word-break:keep-all;'>\n" +
-                "    <tbody>\n" +
-                "    <tr>\n" +
-                "        <td style='padding:0 0 0 0; border-top:none; border-bottom:none; border-left:none; border-right:none;'>\n" +
-                "            <table align='center' border='0' cellpadding='0' cellspacing='0'\n" +
-                "                   style='padding:0px 0px 0px 0px; width:100%; max-width:800px; margin:0 auto; background:#fff;'>\n" +
-                "                <tbody>\n" +
-                "                <tr>\n" +
-                "                    <td style='border:10px solid #f2f2f2; padding:90px 14px 90px 14px; margin-left:auto; margin-right:auto;'>\n" +
-                "                        <table align='center' border='0' cellpadding='0' cellspacing='0'\n" +
-                "                               style='width:100%; max-width:630px; margin-left:auto; margin-right:auto; letter-spacing:-1px;'>\n" +
-                "                            <tbody>\n" +
-                "                            <tr>\n" +
-                "                                <td style='font-size:44px;line-height:48px;font-weight:bold;color:#000000;padding-bottom:60px;text-align:left;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>\n" +
-                "                                    <span>우리가 이메일 인증 코드</span>\n" +
-                "                                </td>\n" +
-                "                            </tr>\n" +
-                "                            <tr>\n" +
-                "                                <td style='padding-bottom:50px;font-size:14px;line-height:22px;font-weight:normal;color:#000000;text-align:left;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>\n" +
-                "                                    <p style='margin:0;padding:0;'>공유 가계부 우리가 회원 가입 과정 중, 사용자 인증을 위해\n" +
-                "                                        발송된 이메일입니다. </p>\n" +
-                "                                    <p style='margin:0;padding:0;'>인증 과정을 완료하기 위해 아래 인증 코드를 입력해주세요.</p>\n" +
-                "                                </td>\n" +
-                "                            </tr>\n" +
-                "                            <tr>\n" +
-                "                                <td style='padding-bottom:50px;'>\n" +
-                "                                    <table align='center' border='0' cellpadding='0' cellspacing='0'\n" +
-                "                                           style='width:100%;'>\n" +
-                "                                        <tbody>\n" +
-                "                                        <tr>\n" +
-                "                                            <td style='padding:50px 30px;border:1px solid #eeeeee;background:#fbfbfb;text-align:center;'>\n" +
-                "                                                <p style='margin:0;padding:0;font-size:18px;font-weight:bold;color:#000000;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>";
+        msg += """
+                <meta charset='UTF-8'>
+                <table align='center' border='0' cellpadding='0' cellspacing='0' width='100%'
+                       style='padding:60px 0 60px 0;color:#555;font-size:16px;word-break:keep-all;'>
+                    <tbody>
+                    <tr>
+                        <td style='padding:0 0 0 0; border-top:none; border-bottom:none; border-left:none; border-right:none;'>
+                            <table align='center' border='0' cellpadding='0' cellspacing='0'
+                                   style='padding:0px 0px 0px 0px; width:100%; max-width:800px; margin:0 auto; background:#fff;'>
+                                <tbody>
+                                <tr>
+                                    <td style='border:10px solid #f2f2f2; padding:90px 14px 90px 14px; margin-left:auto; margin-right:auto;'>
+                                        <table align='center' border='0' cellpadding='0' cellspacing='0'
+                                               style='width:100%; max-width:630px; margin-left:auto; margin-right:auto; letter-spacing:-1px;'>
+                                            <tbody>
+                                            <tr>
+                                                <td style='font-size:44px;line-height:48px;font-weight:bold;color:#000000;padding-bottom:60px;text-align:left;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>
+                                                    <span>우리가 이메일 인증 코드</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style='padding-bottom:50px;font-size:14px;line-height:22px;font-weight:normal;color:#000000;text-align:left;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>
+                                                    <p style='margin:0;padding:0;'>공유 가계부 우리가 회원 가입 과정 중, 사용자 인증을 위해
+                                                        발송된 이메일입니다. </p>
+                                                    <p style='margin:0;padding:0;'>인증 과정을 완료하기 위해 아래 인증 코드를 입력해주세요.</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style='padding-bottom:50px;'>
+                                                    <table align='center' border='0' cellpadding='0' cellspacing='0'
+                                                           style='width:100%;'>
+                                                        <tbody>
+                                                        <tr>
+                                                            <td style='padding:50px 30px;border:1px solid #eeeeee;background:#fbfbfb;text-align:center;'>
+                                                                <p style='margin:0;padding:0;font-size:18px;font-weight:bold;color:#000000;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>""";
         msg += ePw;
-        msg += "</p>\n" +
-                "                                            </td>\n" +
-                "                                        </tr>\n" +
-                "                                        </tbody>\n" +
-                "                                    </table>\n" +
-                "                                </td>\n" +
-                "                            </tr>\n" +
-                "                            <tr>\n" +
-                "                                <td style='font-size:14px;line-height:22px;font-weight:normal;color:#000000;text-align:left;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>\n" +
-                "                                    <p style='margin:0;padding:0;'>감사합니다.<br>우리가</p>\n" +
-                "                                </td>\n" +
-                "                            </tr>\n" +
-                "                            </tbody>\n" +
-                "                        </table>\n" +
-                "                    </td>\n" +
-                "                </tr>\n" +
-                "                </tbody>\n" +
-                "            </table>\n" +
-                "        </td>\n" +
-                "    </tr>\n" +
-                "    </tbody>\n" +
-                "</table>";
+        msg += """
+                </p>
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style='font-size:14px;line-height:22px;font-weight:normal;color:#000000;text-align:left;letter-spacing:-1px;font-family:나눔고딕, NanumGothic, 맑은고딕, Malgun Gothic, 돋움, Dotum, Helvetica, Apple SD Gothic Neo, Sans-serif;'>
+                                                    <p style='margin:0;padding:0;'>감사합니다.<br>우리가</p>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>""";
         mimeMessage.setText(msg, "utf-8", "html");
         mimeMessage.setFrom(new InternetAddress("uliga_dev_team@naver.com", "ULIGA_admin"));
         return mimeMessage;
@@ -143,7 +145,7 @@ public class EmailCertificationService {
             valueOperations.set(to, ePw);
             redisTemplate.expire(to, EMAIL_CERTIFICATION_TIME, TimeUnit.MILLISECONDS);
         } catch (MailException es) {
-            log.info(es.getLocalizedMessage());
+            log.info(es.getMessage());
             throw new IllegalArgumentException(es.getMessage());
         }
 
@@ -279,7 +281,7 @@ public class EmailCertificationService {
         }
 
         CodeConfirmDto confirmDto = CodeConfirmDto.builder().build();
-        confirmDto.setMatches(code.equals(emailConfirmCodeDto.getCode()) || emailConfirmCodeDto.getCode().equals("000000"));
+        confirmDto.setMatches(code.equals(emailConfirmCodeDto.getCode()));
         valueOperations.getAndDelete(emailConfirmCodeDto.getEmail());
         return confirmDto;
 
