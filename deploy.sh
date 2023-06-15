403c746456b2e00b146108b26ae406389089dde8
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-06-15 16:23:15 +0900

    [ADD] deploy.sh profile check

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ echo "> $CURRENT_PROFILE"
 touch ./src/main/resources/application.yml
 
 # profile-application.yml 파일 생성
-touch ./src/main/resources//profile-application.yml
+touch ./src/main/resources/profile-application.yml
 
 # GitHub-Actions 에서 설정한 값을 application.yml 파일에 쓰기
-echo "${{ secrets.APPLICATION }}" >> ./src/main/resources//application.yml
+echo "${{ secrets.APPLICATION }}" >> ./src/main/resources/application.yml
 
-echo "${{ secrets.PROFILE_APPLICATION }}" >> ./src/main/resources//profile-application.yml
+echo "${{ secrets.PROFILE_APPLICATION }}" >> ./src/main/resources/profile-application.yml
