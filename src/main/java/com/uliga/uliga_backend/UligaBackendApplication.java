214f135ea890fbd5426a4fdae1387de200a71de4
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-06-18 16:38:30 +0900

    [ADD] 무중단 배포 확인

--- src/main/java/com/uliga/uliga_backend/UligaBackendApplication.java
+++ src/main/java/com/uliga/uliga_backend/UligaBackendApplication.java
@@ -1,23 +1,17 @@
 package com.uliga.uliga_backend;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class UligaBackendApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:profile-application.yml";
 
 
     public static void main(String[] args) {
-//        SpringApplication.run(UligaBackendApplication.class, args);
-        new SpringApplicationBuilder(UligaBackendApplication.class).properties(APPLICATION_LOCATIONS).run(args);
+        SpringApplication.run(UligaBackendApplication.class, args);
     }
 
 }
