8ce8f235a05109b596139949067b0d9330defeb1
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-06-19 20:18:40 +0900

    [ADD] 엑세스 토큰 유효시간 1시간 -> 10분

--- src/main/java/com/uliga/uliga_backend/global/common/constants/JwtConstants.java
+++ src/main/java/com/uliga/uliga_backend/global/common/constants/JwtConstants.java
@@ -3,7 +3,7 @@ package com.uliga.uliga_backend.global.common.constants;
 public class JwtConstants {
     public static final String AUTHORITIES_KEY = "auth";
     public static final String BEARER_TYPE = "Bearer";
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60;           // 하루 1000 * 60 * 60 * 24
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 10;           // 10분 1000 * 60 * 10
     public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일 1000 * 60 * 60 * 24 * 7
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
