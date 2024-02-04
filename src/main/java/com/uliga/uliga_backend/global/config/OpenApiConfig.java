ed2ea171e91b2a052fddeda1c540b4e535a72155
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-02-04 17:22:16 +0900

    Feat : swagger 설정 수정

--- src/main/java/com/uliga/uliga_backend/global/config/OpenApiConfig.java
+++ src/main/java/com/uliga/uliga_backend/global/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.uliga.uliga_backend.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -13,8 +14,10 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(title = "<우리가>",
                 description = "공유 가계부 <우리가> API 명세서",
                 version = "1.0"
-        )
-
+        ),
+    servers = {
+            @Server(url = "https://uliga.site", description = "server")
+    }
 )
 @RequiredArgsConstructor
 @Configuration
