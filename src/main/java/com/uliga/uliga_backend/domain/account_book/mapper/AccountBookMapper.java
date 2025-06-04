8136f93b47432bfd931b53ded334d16c095153d1
kim-dong-jun <kim.dongjun@linkareer.com>
2025-06-04 22:42:27 +0900

    🔥 사용되지 않을 모델, dao 관련 코드들 제거

--- src/main/java/com/uliga/uliga_backend/domain/account_book/mapper/AccountBookMapper.java
+++
@@ -1,7 +0,0 @@
-package com.uliga.uliga_backend.domain.account_book.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AccountBookMapper {
-}
--- src/main/java/com/uliga/uliga_backend/domain/account_book_data/model/AccountBookDataType.java
+++
@@ -1,5 +0,0 @@
-package com.uliga.uliga_backend.domain.account_book_data.model;
-
-public enum AccountBookDataType {
-    RECORD, INCOME
-}
