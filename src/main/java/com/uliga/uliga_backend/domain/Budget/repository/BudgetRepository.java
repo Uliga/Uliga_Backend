9c8a7be79c113c6bc692b966df86acd2dd96db53
kim-dong-jun <kim.dongjun@linkareer.com>
2025-06-05 17:06:07 +0900

    ♻️ 이전 레포지토리 로직 주석 추가

--- src/main/java/com/uliga/uliga_backend/domain/Budget/repository/BudgetRepository.java
+++ src/main/java/com/uliga/uliga_backend/domain/Budget/repository/BudgetRepository.java
@@ -12,4 +12,26 @@ import lombok.RequiredArgsConstructor;
 public class BudgetRepository {
   private final BudgetDao dao;
   private final DSLContext dsl;
+
+  /*
+   * @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.MonthlySumQ("
+   * +
+   * "SUM(b.value)" +
+   * ") FROM AccountBook ab " +
+   * "JOIN Budget b ON b.accountBook.id = ab.id " +
+   * "WHERE ab.id=:id " +
+   * "AND b.month=:month " +
+   * "AND b.year = :year " +
+   * "GROUP BY ab.id")
+   * 
+   * Optional<MonthlySumQ> getMonthlySumByAccountBookId(@Param("id") Long
+   * id, @Param("year") Long year, @Param("month") Long month);
+   * 
+   * boolean existsBudgetByAccountBookIdAndYearAndMonth(Long accountBookId, Long
+   * year, Long month);
+   * 
+   * Optional<Budget> findByAccountBookIdAndYearAndMonth(Long accountBookId, Long
+   * year, Long month);
+   * 
+   */
 }
