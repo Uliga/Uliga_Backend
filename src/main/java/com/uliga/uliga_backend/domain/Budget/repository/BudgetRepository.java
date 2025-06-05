abfa587b225122c54e6e9de4238cbee8d78902f6
kim-dong-jun <kim.dongjun@linkareer.com>
2025-06-05 13:51:33 +0900

    ♻️ BudgetRepository migrate

--- src/main/java/com/uliga/uliga_backend/domain/Budget/repository/BudgetRepository.java
+++ src/main/java/com/uliga/uliga_backend/domain/Budget/repository/BudgetRepository.java
@@ -1,27 +1,15 @@
 package com.uliga.uliga_backend.domain.budget.repository;
 
-import com.uliga.uliga_backend.domain.account_book_data.dto.NativeQ.MonthlySumQ;
-import com.uliga.uliga_backend.domain.budget.model.Budget;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import com.uliga.uliga_backend.jooq.tables.daos.BudgetDao;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
-public interface BudgetRepository extends JpaRepository<Budget, Long> {
-    @Query("SELECT NEW com.uliga.uliga_backend.domain.AccountBookData.dto.NativeQ.MonthlySumQ(" +
-            "SUM(b.value)" +
-            ") FROM AccountBook ab " +
-            "JOIN Budget b ON b.accountBook.id = ab.id " +
-            "WHERE ab.id=:id " +
-            "AND b.month=:month " +
-            "AND b.year = :year " +
-            "GROUP BY ab.id")
-            
-    Optional<MonthlySumQ> getMonthlySumByAccountBookId(@Param("id") Long id, @Param("year") Long year, @Param("month") Long month);
-
-    boolean existsBudgetByAccountBookIdAndYearAndMonth(Long accountBookId, Long year, Long month);
-
-    Optional<Budget> findByAccountBookIdAndYearAndMonth(Long accountBookId, Long year, Long month);
+@Repository
+@RequiredArgsConstructor
+public class BudgetRepository {
+  private final BudgetDao dao;
+  private final DSLContext dsl;
 }
