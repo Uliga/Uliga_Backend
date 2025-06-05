466dffe222735e719e4fa55def5eefbac0ab64fb
kim-dong-jun <kim.dongjun@linkareer.com>
2025-06-05 23:51:25 +0900

    ✨ ExpenseCategoryController 추가

--- src/main/java/com/uliga/uliga_backend/domain/Budget/api/BudgetControllerV2.java
+++ src/main/java/com/uliga/uliga_backend/domain/Budget/api/BudgetControllerV2.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,6 @@ import com.uliga.uliga_backend.global.common.annotation.Serialize;
 import com.uliga.uliga_backend.jooq.tables.pojos.Budget;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
