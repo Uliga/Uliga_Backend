967e9d635c0f1d26aa1bdce1c6fa820538b618e5
kim-dong-jun <kim.dongjun@linkareer.com>
2025-06-06 15:37:41 +0900

    ✨ Common request, response dto 추가

---
+++ src/main/java/com/uliga/uliga_backend/global/common/DatePeriod.java
@@ -0,0 +1,15 @@
+package com.uliga.uliga_backend.global.common;
+
+import lombok.Getter;
+
+@Getter
+public enum DatePeriod {
+  DAILY("date"), WEEKLY("week"), MONTHLY("month"), YEARLY("year");
+
+  private String name;
+
+  DatePeriod(String name) {
+    this.name = name;
+  }
+
+}
