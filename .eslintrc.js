1ce6c19b30ffd2d18a70cbbd42033ace2eb0c002
kdjun99 <dongjunkim99@icloud.com>
2025-06-13 22:53:50 +0900

    💄 Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     es2022: true,
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   ignorePatterns: ['dist', 'node_modules', '.eslintrc.js'],
